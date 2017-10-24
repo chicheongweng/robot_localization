9d6024870f8b6e44ac6a6073170ad13e28004450
chicheongweng <chicheongweng@gmail.com>
2017-10-24 12:18:24 +0800

    updated inference.py

--- inference.py
+++ inference.py
@@ -60,15 +60,70 @@ def forward_backward(observations):
     #
 
     num_time_steps = len(observations)
-    forward_messages = [None] * num_time_steps
-    forward_messages[0] = [prior_distribution]
-    # TODO: Compute the forward messages
+    # forward_messages[i] is P([state at i] and y's before i)
+    forward_messages = [prior_distribution]
+    for t in range(num_time_steps - 1):
+        # Incorporate the t-th observation.
+        if observations[t] is not None:
+            prob_given_y = robot.Distribution()
+            for state, prob in forward_messages[t].items():
+                prob_given_y[state] \
+                    = prob * observation_model(state)[observations[t]]
+            prob_given_y.renormalize()
+        else:
+            prob_given_y = forward_messages[t]
 
+        # Take a step forward in time, using the transition model.
+        next_dist = robot.Distribution()
+        for state, prob in prob_given_y.items():
+            for new_state, trans_prob in transition_model(state).items():
+                next_dist[new_state] += prob * trans_prob
+        next_dist.renormalize()
+        forward_messages.append(next_dist)
+
+    # Make the reverse transition dictionary, to make backward messages
+    # easier.
+    backwards_transitions = collections.defaultdict(robot.Distribution)
+    for state_1 in all_possible_hidden_states:
+        for state_2, prob in transition_model(state_1).items():
+            backwards_transitions[state_2][state_1] += prob
+
+    # backward messages[i] is P(y's after i | state at i)
     backward_messages = [None] * num_time_steps
-    # TODO: Compute the backward messages
+    uniform = robot.Distribution()
+    for s in all_possible_hidden_states:
+        uniform[s] = 1
+    uniform.renormalize()
+    backward_messages[-1] = uniform
+    for t in range(num_time_steps - 1, 0, -1):
+        if observations[t] is not None:
+            prob_given_y = robot.Distribution()
+            for state, prob in backward_messages[t].items():
+                prob_given_y[state] = prob * \
+                    observation_model(state)[observations[t]]
+            prob_given_y.renormalize()
+        else:
+            prob_given_y = backward_messages[t]
 
-    marginals = [None] * num_time_steps # remove this
-    # TODO: Compute the marginals 
+        prev_dist = robot.Distribution()
+        for state, prob in prob_given_y.items():
+            for past_state, trans_prob in backwards_transitions[state].items():
+                prev_dist[past_state] += prob * trans_prob
+        prev_dist.renormalize()
+        backward_messages[t - 1] = prev_dist
+
+    # Finally, compute marginals.
+    marginals = []
+    for t in range(num_time_steps):
+        marginal_t = robot.Distribution()
+        for state in all_possible_hidden_states:
+            product = forward_messages[t][state] * backward_messages[t][state]
+            if observations[t] is not None:
+                product *= observation_model(state)[observations[t]]
+            if product > 0:
+                marginal_t[state] = product
+        marginal_t.renormalize()
+        marginals.append(marginal_t)
 
     return marginals
 
@@ -90,11 +145,48 @@ def Viterbi(observations):
     # YOUR CODE GOES HERE
     #
 
+    # forward[t][s] represents the MAP estimate of the state of the robot
+    # in timestep t, assuming the robot is in state s.  It contains (logprob,
+    # old_s), the latter of which points to the state in time t-1 that
+    # is part of this MAP estimate.
+    forward = []
+    initial_map = {}
+    for s in all_possible_hidden_states:
+        initial_map[s] = [careful_log(prior_distribution[s]), None]
+        if observations[0] is not None:
+            initial_map[s][0] += \
+                careful_log(observation_model(s)[observations[0]])
+    forward.append(initial_map)
 
-    num_time_steps = len(observations)
-    estimated_hidden_states = [None] * num_time_steps # remove this
+    n_steps = len(observations)
+    for t in range(1, n_steps):
+        # In each timestep, first apply transition probabilities from the
+        # previous timestep.
+        new_map = collections.defaultdict(lambda: (-np.inf, None))
+        for old_s, (old_prob, _) in forward[-1].items():
+            for new_s, trans_prob in transition_model(old_s).items():
+                total_prob = old_prob + careful_log(trans_prob)
+                # Update new MAP estimate if necessary.
+                if total_prob > new_map[new_s][0]:
+                    new_map[new_s] = (total_prob, old_s)
+        # Then, apply observation probabilities.
+        if observations[t] is not None:
+            for s in all_possible_hidden_states:
+                log_prob, back_ptr = new_map[s]
+                new_map[s] = (
+                    log_prob +
+                    careful_log(observation_model(s)[observations[t]]),
+                    back_ptr)
+        forward.append(new_map)
 
-    return estimated_hidden_states
+    # Read off the answer using the back pointers.
+    MAP_estimate = [None] * n_steps
+    MAP_estimate[n_steps - 1] = max(forward[n_steps - 1],
+                                    key=lambda s: forward[n_steps - 1][s][0])
+    for t in range(n_steps - 2, -1, -1):
+        MAP_estimate[t] = forward[t + 1][MAP_estimate[t + 1]][1]
+
+    return MAP_estimate
 
 
 def second_best(observations):
@@ -114,9 +206,153 @@ def second_best(observations):
     # YOUR CODE GOES HERE
     #
 
-
     num_time_steps = len(observations)
-    estimated_hidden_states = [None] * num_time_steps # remove this
+
+    # Basically for each (possible) hidden state at time step i, we need to
+    # keep track of the best previous hidden state AND the second best
+    # previous hidden state--where we need to keep track of TWO back pointers
+    # per (possible) hidden state at each time step!
+
+    messages = []  # best values so far
+    messages2 = []  # second-best values so far
+    back_pointers = []  # per time step per hidden state, we now need
+    # *two* back-pointers
+
+    # -------------------------------------------------------------------------
+    # Fold observations into singleton potentials
+    #
+    phis = []  # phis[n] is the singleton potential for node n
+    for n in range(num_time_steps):
+        potential = robot.Distribution()
+        observed_state = observations[n]
+        if n == 0:
+            for hidden_state in prior_distribution:
+                value = prior_distribution[hidden_state]
+                if observed_state is not None:
+                    value *= observation_model(hidden_state)[observed_state]
+                if value > 0:  # only store entries with nonzero prob.
+                    potential[hidden_state] = value
+        else:
+            for hidden_state in all_possible_hidden_states:
+                if observed_state is None:
+                    # singleton potential should be identically 1
+                    potential[hidden_state] = 1.
+                else:
+                    value = observation_model(hidden_state)[observed_state]
+                    if value > 0:  # only store entries with nonzero prob.
+                        potential[hidden_state] = value
+        phis.append(potential)
+
+    # -------------------------------------------------------------------------
+    # Forward pass
+    #
+
+    # handle initial time step differently
+    initial_message = {}
+    for hidden_state in prior_distribution:
+        value = -careful_log(phis[0][hidden_state])
+        if value < np.inf:  # only store entries with nonzero prob.
+            initial_message[hidden_state] = value
+    messages.append(initial_message)
+    initial_message2 = {}  # there is no second-best option
+    messages2.append(initial_message2)
+
+    # rest of the time steps
+    for n in range(1, num_time_steps):
+        prev_message = messages[-1]
+        prev_message2 = messages2[-1]
+        new_message = {}
+        new_message2 = {}
+        new_back_pointers = {}  # need to store 2 per possible hidden state
+
+        # only look at possible hidden states given observation
+        for hidden_state in phis[n]:
+            values = []
+            # each entry in values will be a tuple of the form:
+            # (<value>, <previous hidden state>,
+            #  <which back pointer we followed>),
+            # where <which back pointer we followed> is 0 (best back pointer)
+            # or 1 (second-best back pointer)
+
+            # iterate through best previous values
+            for prev_hidden_state in prev_message:
+                value = prev_message[prev_hidden_state] - \
+                    careful_log(transition_model(prev_hidden_state)[
+                        hidden_state]) - \
+                    careful_log(phis[n][hidden_state])
+                if value < np.inf:
+                    # only store entries with nonzero prob.
+                    values.append((value, prev_hidden_state, 0))
+
+            # also iterate through second-best previous values
+            for prev_hidden_state in prev_message2:
+                value = prev_message2[prev_hidden_state] - \
+                    careful_log(transition_model(prev_hidden_state)[
+                        hidden_state]) - \
+                    careful_log(phis[n][hidden_state])
+                if value < np.inf:
+                    # only store entries with nonzero prob.
+                    values.append((value, prev_hidden_state, 1))
+
+            if len(values) > 0:
+                # this part could actually be sped up by not using a sorting
+                # algorithm...
+                sorted_values = sorted(values, key=lambda x: x[0])
+                best_value, best_prev_hidden_state, which_back_pointer = \
+                    sorted_values[0]
+
+                # for the best value, the back pointer should *always* be 0,
+                # meaning that we follow the best back pointer and not the
+                # second best
+                if len(values) > 1:
+                    best_value2, best_prev_hidden_state2, which_back_pointer2\
+                        = sorted_values[1]
+                else:
+                    best_value2 = np.inf
+                    best_prev_hidden_state2 = None
+                    which_back_pointer2 = None
+
+                new_message[hidden_state] = best_value
+                new_message2[hidden_state] = best_value2
+                new_back_pointers[hidden_state] = \
+                    ((best_prev_hidden_state, which_back_pointer),
+                     (best_prev_hidden_state2, which_back_pointer2))
+
+        messages.append(new_message)
+        messages2.append(new_message2)
+        back_pointers.append(new_back_pointers)
+
+    # -------------------------------------------------------------------------
+    # Backward pass (follow back-pointers)
+    #
+
+    # handle last time step differently
+    values = []
+    for hidden_state, value in messages[-1].items():
+        values.append((value, hidden_state, 0))
+    for hidden_state, value in messages2[-1].items():
+        values.append((value, hidden_state, 1))
+
+    divergence_time_step = -1
+
+    if len(values) > 1:
+        # this part could actually be sped up by not using a sorting
+        # algorithm...
+        sorted_values = sorted(values, key=lambda x: x[0])
+
+        second_best_value, hidden_state, which_back_pointer = sorted_values[1]
+        estimated_hidden_states = [hidden_state]
+
+        # rest of the time steps
+        for t in range(num_time_steps - 2, -1, -1):
+            hidden_state, which_back_pointer = \
+                back_pointers[t][hidden_state][which_back_pointer]
+            estimated_hidden_states.insert(0, hidden_state)
+    else:
+        # this happens if there isn't a second best option, which should mean
+        # that the only possible option (the MAP estimate) is the only
+        # solution with 0 error
+        estimated_hidden_states = [None] * num_time_steps
 
     return estimated_hidden_states
 
